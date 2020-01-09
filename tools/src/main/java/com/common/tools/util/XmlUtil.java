3bade7f502202562f0b86cb44c4ee9b15e763ab1
gongliangjun <liangjun.gong@hand-china.com>
2020-01-09 20:54:05 +0800

    feat(工具):添加bean 2 xml

--- tools/src/main/java/com/common/tools/util/XmlUtil.java
+++ tools/src/main/java/com/common/tools/util/XmlUtil.java
@@ -1,5 +1,13 @@
 package com.common.tools.util;
 
+import com.common.tools.util.pojo.Msg;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.dom4j.*;
 import org.dom4j.io.OutputFormat;
 import org.dom4j.io.SAXReader;
@@ -12,6 +20,7 @@ import java.util.List;
 /**
  * xml解析工具类
  */
+@Slf4j
 public class XmlUtil {
 
 	/**
@@ -126,4 +135,50 @@ public class XmlUtil {
 			}
 		}
 	}
+
+	/**
+	 * bean 转xml
+	 *
+	 * @param bean
+	 * @param ignoreNull
+	 * @param propertyNamingStrategy
+	 * @author gongliangjun 2020-01-09 8:53 PM
+	 * @return java.lang.String
+	 */
+	public static String beanToXml(Object bean, boolean ignoreNull, PropertyNamingStrategy propertyNamingStrategy) {
+		XmlMapper xmlMapper = new XmlMapper();
+		xmlMapper.setDefaultUseWrapper(false);
+		//字段为null，自动忽略，不再序列化
+		if (ignoreNull) {
+			xmlMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+		}
+		//XML标签名:使用骆驼命名的属性名，
+		if(null != propertyNamingStrategy){
+			xmlMapper.setPropertyNamingStrategy(propertyNamingStrategy);
+		}
+		//设置转换模式
+		xmlMapper.enable(MapperFeature.USE_STD_BEAN_NAMING);
+		try {
+			return xmlMapper.writeValueAsString(bean);
+		} catch (JsonProcessingException e) {
+			log.error("beanToXml error:{}", e);
+		}
+		return StringUtils.EMPTY;
+	}
+
+	public static void main(String[] args) {
+
+
+		Msg msg = new Msg();
+		msg.setMsgTemplate("aaaa");
+		String s = beanToXml(msg, false, PropertyNamingStrategy.LOWER_CAMEL_CASE);
+		s = beanToXml(msg, false, PropertyNamingStrategy.KEBAB_CASE);
+		s = beanToXml(msg, false, PropertyNamingStrategy.LOWER_CASE);
+		s = beanToXml(msg, false, PropertyNamingStrategy.LOWER_DOT_CASE);
+		s = beanToXml(msg, false, PropertyNamingStrategy.SNAKE_CASE);
+		s = beanToXml(msg, false, PropertyNamingStrategy.UPPER_CAMEL_CASE);
+		System.out.println(s);
+
+	}
+
 }
